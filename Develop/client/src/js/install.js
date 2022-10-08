34f3843c37c2cb70952603bc91536c4e9510d496
JM <georgemares14@yahoo.com>
2022-10-08 00:23:26 -0700

    Added  prevent default to install.js event listener

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -3,6 +3,7 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
     window.deferredPrompt = event;
 
     // Remove the hidden class from the button.
@@ -12,15 +13,13 @@ window.addEventListener('beforeinstallprompt', (event) => {
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
-    // console.log(promptEvent)
     if (!promptEvent) {
         return;
     }
 
-    // Show prompt
+    
     promptEvent.prompt();
 
-    // Reset the deferred prompt variable, it can only be used once.
     window.deferredPrompt = null;
 
     butInstall.classList.toggle('hidden', true);

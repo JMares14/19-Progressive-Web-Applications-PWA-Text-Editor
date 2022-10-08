bf19911995f689fa34bdc5c065ccbad8f98726de
JM <georgemares14@yahoo.com>
2022-10-07 23:03:34 -0700

    Finished TODOS started debugging and testing

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1);
+  const object = jateDB.transaction('jate', 'readwrite')
+  const store = object.objectStore('jate');
+  const request = store.add({main: content})
+  const results = await request;
+  return results;
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  const jateDB = await openDB('jate', 1);
+  const object = jateDB.transaction('jate', 'readwrite')
+  const store = object.objectStore('jate');
+  const request = store.getAll();
+  const results = await request;
+  return results;
+};
 initdb();
